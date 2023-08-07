e9f7c2484f79862d68715fece89372a3618a79d8
Arangarajan <arangarajanmoorthy002@gmail.com>
2023-08-06 21:27:58 -0700

    comp guess proj added

---
+++ comp_guessno.py
@@ -0,0 +1,15 @@
+import random
+def cguess():
+    given_no=int(input("Enter the no so comp can guess : "))
+    random_no=random.randint(1,20)
+    if given_no != random_no:
+        if given_no < random_no:
+            while given_no < random_no:
+                print("if",random_no)
+                random_no=random_no-1
+        elif given_no > random_no:
+            while given_no < random_no:
+                print("else",random_no)
+                random_no=random_no+1
+    print(f"Haha don't mess me i am a computer ur gven no is {random_no}")
+cguess()

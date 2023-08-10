4f8788df7a8ad93085a47e3cbc17ee351022aa95
Arangarajan <arangarajanmoorthy002@gmail.com>
2023-08-10 23:09:52 +0530

    convert user input into smaller

--- stonepaper.py
+++ stonepaper.py
@@ -19,7 +19,7 @@ def play():
     p=0
     while p == 0:
 
-        user=input("Enter R or S or P : ")
+        user=input("Enter R or S or P : ").lower()
         
         comp=random.choice(['r','s','p'])
         print(comp)
@@ -27,14 +27,14 @@ def play():
             print('Tie')
         if win(user,comp):
             print('U won') 
-        else:    
+        elif():    
             print('Ulost " :) " ')
         p=int(input("Do you want to continue enter:'0': "))    
     else:
-        print("Hope u enjoy Thank u")
+        print("Hope u enjoy Thank u !!!")
         exit()
 def win(useer,copl):
-    
+    useer.lower()
     #user coplayer
     if((useer=='p' and copl =='r') or (useer=='s' and copl =='p') or (useer=='r' and copl =='s')):
         return True

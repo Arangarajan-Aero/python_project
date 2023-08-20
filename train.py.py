4267c8cc70f263df595ebab0d728c6741bab7f7d
Arangarajan <arangarajanmoorthy002@gmail.com>
2023-08-20 19:24:34 +0530

    new proj added

--- train.py.py
+++ train.py.py
@@ -20,18 +20,23 @@ if Arival=="trichy" or "Trichy" and Dest=="coimbatore" or "Coimbatore":
         T_date=datetime.datetime.now()
         for i in range(pass_count):
             pass_Nm=input(f"Enter name: ")
-            pass_age=int(input("Enter Age "))
+            pass_age=input(f"Enter Age ")
             pass_seat=input(f"Enter lower or upper")
-            seat_no=random.randint(1,50)
-            pass_list.append(pass_Nm)
-            pass_list.append(pass_age)
-            pass_list.append(pass_seat)
+            seat=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17]
+            print("Available seat",seat,end=' ')
+            print("\n")
+            seat_no=input("Seatno: ")
+            pass_list.extend(pass_Nm)
+            pass_list.extend(pass_age)
+            pass_list.extend(pass_seat)
             pass_list.append(seat_no)
-            for j in pass_list:
-                print(f"date :{T_date}")
-                print(f"Name :{pass_Nm} seat No: {seat_no} seatType : {pass_seat}")
-                
-                break
+            seat.remove(seat_no)
+
+        for j in pass_list:
+            print(f"date :{T_date}")
+            print(f"Name :{pass_Nm}  seat No: {seat_no}  seatType : {pass_seat}")
+            print("Your ticket has been successfully Booked")
+            break
                 
     
                     
